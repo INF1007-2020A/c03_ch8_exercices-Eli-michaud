e650820d712fb9990404f41edfe25a84df7d64c9
Eli-michaud <47183913+Eli-michaud@users.noreply.github.com>
2020-10-26 15:36:59 -0400

    ex 1 et 5

--- exercice.py
+++ exercice.py
@@ -3,13 +3,38 @@
 
 
 # TODO: Importez vos modules ici
-
-
-
+from os.path import getsize
 # TODO: Définissez vos fonction ici
+
+def compare_files(fname1, fname2):
+    with open(fname1, 'r') as f, open(fname2, 'r') as p:
+        if f.getsize() != p.getsize():
+            print('Les fichiers ne sont pas de la même taille')
+        else:
+            c, k = f.read(1), p.read(1)
+            while c != '' and k != '':
+                if c != k:
+                    position = f.tell()
+                    print(f"la différence à la position {position} est {c} et {k}")
+                    break
+                c, k = f.read(1), p.read(1)
+
+def trouver_nombre_ordre_croissant(fname):
+    with open(fname, 'r') as f:
+        données = f.read()
+
+    listeNombres = sorted([int(mot) for mot in données.split() if mot.isdigit()])
+
+    #mots = données.split()
+    #for mot in mots:
+     #   if mot.isdigit():
+     #       listeNombres.append(int(mot))
+   # listeNombres.sort()
+    print(listeNombres)
 
 
 if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
+    compare_files('exemple.txt', 'exemple2.txt')
+    trouver_nombre_ordre_croissant('exemple.txt')
 
     pass
